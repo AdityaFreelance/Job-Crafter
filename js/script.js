97e45c150551b4f69d3036d785a52bdf1973dade
AdityaFreelance <149238733+AdityaFreelance@users.noreply.github.com>
2024-02-14 19:06:04 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,68 @@ function stickyHeader() {
         header.classList.remove("sticky");
     }
 }
+
+
+
+// location strip
+$('.locationSlider-container ul').slick({
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+});
+
+// Upcoming Batches slider
+$(document).ready(function () {
+    $('.upcoming-batches').slick({
+        dots: true,
+        arrows:false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    var dotButtons = document.querySelectorAll('.slick-dots li button');
+    dotButtons.forEach(function (button) {
+        button.innerHTML = '';
+    });
+    var carosuelButton = document.querySelectorAll('button.slick-arrow');
+    carosuelButton.forEach(function (button) {
+        button.innerHTML = '';
+    })
+});
